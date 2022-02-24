d8a9239f6378d2520ea7a95dd5510c174e8f3b56
Gieveppix <84123911+Gieveppix@users.noreply.github.com>
2022-02-24 01:25:03 +0100

    Updates

---
+++ precache-manifest.4648445f6bc2db59e5bc5c6eb2cb898e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b164328c94a6ccd19d",
+    "url": "/robofriends/static/css/main.3a137de5.chunk.css"
+  },
+  {
+    "revision": "a9b164328c94a6ccd19d",
+    "url": "/robofriends/static/js/main.a9b16432.chunk.js"
+  },
+  {
+    "revision": "935b0251a626769e024b",
+    "url": "/robofriends/static/js/1.935b0251.chunk.js"
+  },
+  {
+    "revision": "404c8894e13d691f5c7a",
+    "url": "/robofriends/static/css/2.5ea53939.chunk.css"
+  },
+  {
+    "revision": "404c8894e13d691f5c7a",
+    "url": "/robofriends/static/js/2.404c8894.chunk.js"
+  },
+  {
+    "revision": "97713e0cd9366aacb1d6",
+    "url": "/robofriends/static/js/runtime~main.97713e0c.js"
+  },
+  {
+    "revision": "3235c3191c3fb3ee9ee736dffc086044",
+    "url": "/robofriends/index.html"
+  }
+];
